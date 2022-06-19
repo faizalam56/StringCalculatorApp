c5c894f2fc6120a4f9d025f4f97b14c6e3f8d55c
mohammed_alam <mohammed_alam@persistent.com>
2022-06-19 14:33:04 +0530

    string calculator implementation

--- app/src/main/java/com/intuit/stringcalculatorapp/MainActivity.kt
+++ app/src/main/java/com/intuit/stringcalculatorapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.intuit.stringcalculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.intuit.stringcalculatorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+        mBinding.btnAdd.setOnClickListener {
+            addValue()
+        }
+    }
+
+    private fun addValue(){
+        val stringCalculator = StringCalculator()
+
+        val value = mBinding.etValue.text.toString()
+        if (value.isNotEmpty()){
+            if(stringCalculator.isValidInput(value)){
+                mBinding.tvResult.text = stringCalculator.add(value).toString()
+
+            }else{
+                Toast.makeText(this,"Please enter valid data",Toast.LENGTH_LONG).show()
+            }
+        }else Toast.makeText(this,"Please enter value",Toast.LENGTH_LONG).show()
     }
 }
---
+++ app/src/main/java/com/intuit/stringcalculatorapp/StringCalculator.kt
@@ -0,0 +1,55 @@
+package com.intuit.stringcalculatorapp
+
+class StringCalculator {
+
+    fun isValidInput(text: String): Boolean {
+        var text = text
+        return if (text == "") {
+            false
+        } else {
+            var delimiter = ","
+            if (text.matches("//(.*)\n(.*)".toRegex())) {
+                delimiter = text[2].toString()
+                text = text.substring(4)
+            }
+            val numList = splitNumbers(text, "$delimiter|\n")
+            return true
+        }
+    }
+
+    fun add(text: String): Int {
+        var text = text
+        return if (text == "") {
+            0
+        } else {
+            var delimiter = ","
+            if (text.matches("//(.*)\n(.*)".toRegex())) {
+                delimiter = text[2].toString()
+                text = text.substring(4)
+            }
+            val numList = splitNumbers(text, "$delimiter|\n")
+            sum(numList)
+        }
+    }
+
+    private fun toInt(number: String): Int {
+        return number.toInt()
+    }
+
+    private fun splitNumbers(numbers: String, divider: String): Array<String> {
+        return numbers.split(divider).toTypedArray()
+    }
+
+    private fun sum(numbers: Array<String>): Int {
+        var total = 0
+        var negString = ""
+        for (number in numbers) {
+            if (toInt(number) < 0) {
+                if (negString == "") negString = number else negString += ",$number"
+            }
+            if (toInt(number) < 1000) total += toInt(number)
+        }
+        require(negString == "") { "Negatives not allowed: $negString" }
+        return total
+    }
+}
